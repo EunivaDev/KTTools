02dade67ab68622d385ee9cde84708fbf7c539de
Nivek <43946350+NivekUvNart@users.noreply.github.com>
2021-05-25 01:50:13 -0700

    start

--- main.cpp
+++ main.cpp
@@ -1,39 +1,12 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-struct studendRecord
-{
-	int studentID;
-	string name;
-	string major;
-	double GPA;
-
-};
-
-
 int main()
 {
-	const int MAX = 40;
-	HashTable<studendRecord> students(MAX);
+	cout << "Hello World" << endl;
 
-	int size = inputInteger("Enter a number of read-in record (1..." + to_string(MAX) + "): ", 1, MAX);
-
-	//A > Read data file, hash and insert into the dynamic array
-	// students.insert() all number of records
-
-	//B > Find an element from the dynamic array
-	//students.find_index()
-	
-	//C > Insert an element into the dynamic array
-	//students.insert()
-	
-	//D > Remove an element from the dynamic array
-	//students.remove()
-	
-	//E > Display all records from the array
-	cout << students << '\n';
+	system("pause");
 
 	return 0;
 }
