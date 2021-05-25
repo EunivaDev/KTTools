f97d8507a8e9810fc9f60cd0bf5c6ba2f0cd5f5b
Nivek <43946350+NivekUvNart@users.noreply.github.com>
2021-05-25 01:58:32 -0700

    New Main File

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// Developer : Kevin Tran
+// Date Started : 5 / 25 / 2021
+// Last Date Edited : 5 / 25 / 2021
+// Description : Tools files for C++
+
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+    cout << "Hello World" << endl;
+    cout << "Testing Puruposes" << endl;
+
+    system("pause");
+    return 0;
+}
